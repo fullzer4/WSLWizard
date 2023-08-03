4d26f0a13a30973e5bdb42015c676b229a2b30f0
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-03 11:36:31 -0300

    Tensors

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-import Tensor from "./lib/tensor"
 
+import Tensor from './lib/tensor'
+import Types from './lib/types'
+
+class Functions {
+	constructor() {
+		// Tensor creator
+		this.Tensor = Tensor
+
+		// Tensor types
+		this.BFloat16 = Types.BFloat16
+		this.Bool = Types.Bool
+		this.Byte = Types.Byte
+		this.Char = Types.Char
+		this.Double = Types.Double
+		this.Float = Types.Float
+		this.Half = Types.Half
+		this.Int = Types.Int
+		this.Long = Types.Long
+		this.Short = Types.Short
+	}
+}
+
+const jsTorch = new Functions()
+
+export default jsTorch
--- test/tensors.test.js
+++ test/tensors.test.js
@@ -1,6 +1,15 @@
 'use strict'
 
-import core from '../index.js'
+import jsTorch from '../index.js'
 
-import t from "tap"
+import t from 'tap'
 const test = t.test
+
+test('Teste de criação de tensor', (t) => {
+	const data = [[0.11111, 0.222222, 0.3333333]]
+	const dtype = jsTorch.Float
+  
+	const myTensor = jsTorch.tensor(data, dtype)
+
+	t.end()
+})
