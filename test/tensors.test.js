c4d4ebcafcc03113a7f07bf1fbc966416a9bf909
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-05 12:38:28 -0300

    100% coverage

--- test/tensors.test.js
+++ test/tensors.test.js
@@ -5,12 +5,112 @@ import jsTorch from '../index.js'
 
 import t from 'tap'
 
-t.test('Teste de criação de tensor', (t) => {
+t.test('Teste de criação de tensor BFloat16', (t) => {
+	
+	const tensor = jsTorch.Tensor([123], jsTorch.BFloat16)
+	const result = tensor.data()
+
+	t.equal(123, Number(result), `Tensor BFloat16`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Bool', (t) => {
+	
+	const tensor = jsTorch.Tensor([false], jsTorch.Bool)
+	const result = tensor.data()
+
+	t.equal(false, Number(result), `Tensor Bool`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Float', (t) => {
+	
+	const tensor = jsTorch.Tensor([123], jsTorch.Float)
+	const result = tensor.data()
+
+	t.equal(123, Number(result), `Tensor Float`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Double', (t) => {
+	
+	const tensor = jsTorch.Tensor([123], jsTorch.Double)
+	const result = tensor.data()
+
+	t.equal(123, Number(result), `Tensor Double`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Half', (t) => {
+	
+	const tensor = jsTorch.Tensor([123], jsTorch.Half)
+	const result = tensor.data()
+
+	t.equal(123, Number(result), `Tensor Half`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Char', (t) => {
+	
+	const tensor = jsTorch.Tensor(['a'], jsTorch.Char)
+	const result = tensor.data()
+
+	t.equal('a', Number(result), `Tensor Char`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Int', (t) => {
 	
 	const tensor = jsTorch.Tensor([123], jsTorch.Int)
 	const result = tensor.data()
 
-	t.equal(123, Number(result), `Comparacao de resultado`)
+	t.equal(123, Number(result), `Tensor Int`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Byte', (t) => {
+	
+	const tensor = jsTorch.Tensor([1], jsTorch.Byte)
+	const result = tensor.data()
+
+	t.equal(1, Number(result), `Tensor Byte`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Long', (t) => {
+	
+	const tensor = jsTorch.Tensor([12345], jsTorch.Long)
+	const result = tensor.data()
+
+	t.equal(12345, Number(result), `Tensor Long`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Short', (t) => {
+	
+	const tensor = jsTorch.Tensor([123], jsTorch.Short)
+	const result = tensor.data()
+
+	t.equal(123, Number(result), `Tensor Short`)
+
+	t.end()
+})
+
+t.test('Teste de criação de tensor Bool', (t) => {
+	
+	const tensor = jsTorch.Tensor([false], jsTorch.Bool)
+	const result = tensor.data()
+
+	t.equal(false, Number(result), `Tensor Bool`)
 
 	t.end()
 })
