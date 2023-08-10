20dcdcc18e4e4caabf6725efc1370b7d255caf4f
Gabriel Pelizzaro Pereira <gabrielpelizzaro@gmail.com>
2023-08-09 23:11:45 -0300

    Update install.cjs

--- config/install.cjs
+++ config/install.cjs
@@ -1,25 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const https = require('https')
-const fs = require('fs')
-const AdmZip = require('adm-zip')
+const https = require('https');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
 
-export function downloadFile(url, downloadPath, callback) {
-	const file = fs.createWriteStream(downloadPath)
+function downloadFile(url, downloadPath, callback) {
+	const file = fs.createWriteStream(downloadPath);
 
 	https.get(url, response => {
-		response.pipe(file)
+		response.pipe(file);
 		file.on('finish', () => {
-			file.close(callback)
-		})
+			file.close(callback);
+		});
 	}).on('error', error => {
 		fs.unlink(downloadPath, () => {
-			if (callback) callback(error)
-		})
-	})
+			if (callback) callback(error);
+		});
+	});
 }
 
-export function extractZip(zipPath, extractionPath, callback) {
-	const zip = new AdmZip(zipPath)
-	zip.extractAllTo(extractionPath, true)
-	if (callback) callback()
+function extractZip(zipPath, extractionPath, callback) {
+	const zip = new AdmZip(zipPath);
+	zip.extractAllTo(extractionPath, true);
+	if (callback) callback();
 }
+
+module.exports = {
+	downloadFile,
+	extractZip
+};
