6baf8dd4333b87cf789bcd41e7bb78d162bac835
fullzer4 <gabrielpelizzaro@gmail.com>
2024-02-17 21:15:07 -0300

    tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,50 @@
-use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent};
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTraySubmenu};
 use tauri::Manager;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+fn show_main_window(window: tauri::Window) {
+    window.get_window("main").unwrap().show().unwrap();
+} 
 
 fn main() {
-    let tray_menu = SystemTrayMenu::new();
-    let system_tray = SystemTray::new()
-      .with_menu(tray_menu);
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_submenu(
+            SystemTraySubmenu::new("WSLs", SystemTrayMenu::new()
+        ))
+        .add_item(CustomMenuItem::new("quit".to_string(), "Quit"));
+
     tauri::Builder::default()
-      .system_tray(system_tray)
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
-  }
+        .system_tray(SystemTray::new().with_menu(tray_menu).with_id("tray"))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::RightClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a right click");
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                // show_main_window();
+                println!("system tray received a double click");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+                }
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
